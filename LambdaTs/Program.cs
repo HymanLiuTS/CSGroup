65a567dc705b2d1330068b76cada1b33442b8150
刘志辉 <879651072@qq.com>
2017-01-16 10:43:19 +0800

    【C#函数式编程】C#中的函数式编程(一) —— =>表达式

---
+++ LambdaTs/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LambdaTs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> add = (x, y) => x + y;
+            int r = add(1, 2);
+            Console.WriteLine(r);
+
+
+            Func<int, Func<int, int>> add2 = x =>( y => (x + y));
+            r = add2(2)(8);
+            Console.WriteLine(r);
+
+            var add3 = add2(3);
+            Console.WriteLine(add3(0));
+            Console.WriteLine(add3(1));
+
+            Func<int> func = () => 123;
+            Console.WriteLine(func());
+
+            Action<int> print = x => Console.WriteLine(x);
+            print(456);
+
+            Func<SqlConnection, Func<String, DataSet>> Execsql = x => y =>
+            {
+                using (x)
+                {
+                    x.Open();
+                    var com = x.CreateCommand();
+                    DataSet ds = new DataSet();
+                    com.CommandText = y;
+                    SqlDataAdapter adapter = new SqlDataAdapter(com);
+                    adapter.Fill(ds);
+                    return ds;
+                }
+            };
+
+            //Execsql(new SqlConnection("xxx"))("select * from xxx");
+
+            Func<string, Func<SqlConnection, DataSet>> Execsql2 = x => y => Execsql(y)(x);
+
+            //Execsql2("select * from xxx")(new SqlConnection());
+
+            
+        }
+    }
+}
--- LambdaTs/Properties/AssemblyInfo.cs
+++ LambdaTs/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("RefDll")]
+[assembly: AssemblyTitle("LambdaTs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RefDll")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("LambdaTs")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("39a3735f-7ecc-4fd2-a910-3d26c60dcb25")]
+[assembly: Guid("23a8818e-f8b1-4fe0-8e55-3c90e70ed5e0")]
 
 // 程序集的版本信息由下面四个值组成:
 //
